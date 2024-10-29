6270b1d0b04218034a6d4d1a93c2c32a0fd0c9ad
S-andres0694 <sebasandres0694@gmail.com>
2024-10-29 12:46:45 +0000

    feat(testing suite): added support for cypress testing

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,10 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://joanespasa.github.io/PLANLib/');
+  })
+
+  it('should fail', () => {
+    expect(false).to.equal(true);
+  });
+
+})
