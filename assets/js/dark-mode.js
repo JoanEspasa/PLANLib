9f1b85b7ebd7a7c3c3f9522a92aab984b92ebe62
S-andres0694 <sebasandres0694@gmail.com>
2024-11-02 14:28:46 +0000

    feat(homepage): added dark-mode support for the homepage and added a navbar
    - initial rendition of the navbar
    - dark mode switch created
    - tests the dark mode switch successfully

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,70 +1,19 @@
-//Icons used to control the theme.
-const lightIcon = document.querySelector(".light-theme");
-const darkIcon = document.querySelector(".dark-theme");
-
-//Theme variables
-const userPreferenceTheme = localStorage.getItem("theme");
-
-//True if the user preference is dark, false otherwise.
-const systemPreferenceTheme = window.matchMedia(
-  "(prefers-color-scheme: dark)",
-).matches;
-
-//Changes the display if they're there or not.
-function iconToggle() {
-  lightIcon.classList.toggle("hidden");
-  darkIcon.classList.toggle("hidden");
-}
-
-/**
- * Checks if the user has given a previous preference for the dark theme.
- * <br>
- * It also checks for the System Preference to see if it is dark.
- * If true, it adds the dark class on the HTML element and hides the dark icon.
- */
-
-function themeCheck() {
-  if (
-    userPreferenceTheme === "dark" ||
-    (!userPreferenceTheme && systemPreferenceTheme)
-  ) {
-    document.documentElement.classList.add("dark");
-    darkIcon.classList.add("hidden");
-    lightIcon.classList.remove("hidden");
-  } else {
-    lightIcon.classList.add("hidden");
-    darkIcon.classList.remove("hidden");
-  }
-}
-
-/**
- * Manually switches the current theme and updates the preferences accordingly.
- */
-
-function themeSwitch() {
+document.addEventListener('DOMContentLoaded', (event) => {
   const htmlElement = document.documentElement;
-  if (htmlElement.classList.contains("dark")) {
-    htmlElement.classList.remove("dark");
-    localStorage.setItem("theme", "light");
-    iconToggle();
-    return;
-  }
-  htmlElement.classList.add("dark");
-  localStorage.setItem("theme", "dark");
-  iconToggle();
-}
+  const switchElement = document.getElementById('darkModeSwitch');
 
-/*
-Allows switching the themes using the buttons.
- */
+  // Set the default theme to dark if no setting is found in local storage
+  const currentTheme = localStorage.getItem('bsTheme') || 'dark';
+  htmlElement.setAttribute('data-bs-theme', currentTheme);
+  switchElement.checked = currentTheme === 'dark';
 
-lightIcon.addEventListener("click", () => {
-  themeSwitch();
+  switchElement.addEventListener('change', function () {
+    if (this.checked) {
+      htmlElement.setAttribute('data-bs-theme', 'dark');
+      localStorage.setItem('bsTheme', 'dark');
+    } else {
+      htmlElement.setAttribute('data-bs-theme', 'light');
+      localStorage.setItem('bsTheme', 'light');
+    }
+  });
 });
-
-darkIcon.addEventListener("click", () => {
-  themeSwitch();
-});
-
-//Checks the theme on load.
-themeCheck();
